0b7cb8b85ec1f501d99a85bfeb0cc41766b765b2
Romano <romanornr@gmail.com>
2018-10-15 14:23:42 +0200

    Viacoin: [travis] switch Viacoin Core 0.13 to 0.15.2

--- travis/builddeps.sh
+++ travis/builddeps.sh
@@ -17,9 +17,9 @@ git submodule update
 make
 # bitcoind
 cd
-wget https://github.com/viacoin/viacoin/releases/download/v0.13.3.7/viacoin-0.13.3-x86_64-linux-gnu.tar.gz
-echo "f95915761e0c6803697014a484da9370fbf470972f20144fd37c881207703a61 viacoin-0.13.3-x86_64-linux-gnu.tar.gz" > sha256sum.asc
+wget https://github.com/viacoin/viacoin/releases/download/v0.15.2/viacoin-0.15.2-x86_64-linux-gnu.tar.gz
+echo "bdbd432645a8b4baadddb7169ea4bef3d03f80dc2ce53dce5783d8582ac63bab viacoin-0.15.2-x86_64-linux-gnu.tar.gz" > sha256sum.asc
 sha256sum -c sha256sum.asc
-tar xzvf viacoin-0.13.3-x86_64-linux-gnu.tar.gz
+tar xzvf viacoin-0.15.2-x86_64-linux-gnu.tar.gz
 popd
 
